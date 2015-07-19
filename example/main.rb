21a14c123fdb6a8517ccc6470add436375c66f46
RicciFlowing <benjamin@mathe-sellin.de>
2015-07-19 15:57:47 +0200

    Added a new test_text to the example application, to show that also non blender related cg-articles are also correctly categorized

--- example/main.rb
+++ example/main.rb
@@ -6,5 +6,6 @@ Benchmark.bm do |bm|
               classifier = TextClassifier.new()
               classifier.classify("test_texts/blender_2_75a_release_notes")
               classifier.classify("test_texts/senegal_on_wikipedia")
+              classifier.classify("test_texts/3dCG_on_wikipedia")
             }
 end
