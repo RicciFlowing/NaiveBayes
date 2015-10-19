0f7d4bfed1e8ae2e43b0c0ada62dd38b9ce3bf3e
RicciFlowing <benjamin@mathe-sellin.de>
2015-10-19 21:24:04 +0200

    First integration tests for loading trainingsdata via ActiveRecord

--- spec/integration/classification_spec.rb
+++ spec/integration/classification_spec.rb
@@ -2,27 +2,46 @@ require 'spec_helper'
 require 'NaiveText'
 
 describe 'Classification' do
-  let(:categories_config) do
-     [{name: 'interesting', path: 'spec/training/positive'},
-      {name: 'boring', path: 'spec/training/negative'}]
+
+  context 'with trainingsdata as files' do
+    let(:categories_config) do
+       [{name: 'interesting', path: 'spec/training/positive'},
+        {name: 'boring', path: 'spec/training/negative'}]
+    end
+
+    it 'classifies the given text correctly' do
+      classifier = NaiveText.build(categories_config)
+      expect { classifier.classify("test").name }
+    end
+
+    it 'returns a propability collection' do
+      classifier = NaiveText.build(categories_config)
+      expect(classifier.propabilities("test")).to be_instance_of(PropabilityCollection)
+    end
   end
 
-  let(:empty_file) do
-     [{name: 'empty', path: 'spec/training/with_empty_file'},
-      {name: 'boring', path: 'spec/training/negative'}]
-  end
+  context 'with trainingsdata as ActiveRecords' do
+    let(:categories_config) do
+       [{name: 'interesting', models: Test.interesting, property: 'description' },
+        {name: 'boring', models:Test.boring, property: 'description' }]
+    end
 
-  it 'classifies the given text correctly' do
-    classifier = NaiveText.build(categories_config)
-    expect { classifier.classify("test").name }
-  end
+    it 'classifies the given text correctly' do
+      classifier = NaiveText.build(categories_config)
+      expect { classifier.classify("test").name }
+    end
 
-  it 'returns a propability collection' do
-    classifier = NaiveText.build(categories_config)
-    expect(classifier.propabilities("test")).to be_instance_of(PropabilityCollection)
+    xit 'returns a propability collection' do
+      classifier = NaiveText.build(categories_config)
+      expect(classifier.propabilities("test")).to be_instance_of(PropabilityCollection)
+    end
   end
 
   context 'with empty trainings file' do
+    let(:empty_file) do
+       [{name: 'empty', path: 'spec/training/with_empty_file'},
+        {name: 'boring', path: 'spec/training/negative'}]
+    end
     it 'warns the user' do
       expect(STDOUT).to receive(:puts).at_least(1)
       NaiveText.build(empty_file)
