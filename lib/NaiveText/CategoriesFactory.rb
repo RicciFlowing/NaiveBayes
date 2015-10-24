6ceccb6acc9204efe44f8101274e16e28709dbb1
RicciFlowing <benjamin@mathe-sellin.de>
2015-10-24 21:15:10 +0200

    Added support for the new config_category parameters to NaiveText::Build. ReadMe update will follow.

--- lib/NaiveText/CategoriesFactory.rb
+++ lib/NaiveText/CategoriesFactory.rb
@@ -1,18 +1,30 @@
 class CategoriesFactory
   def self.build(config)
     categories = []
-
-    config.each do |category_config|
-      begin
-        examples = ExamplesFactory.from_files(category_config[:path])
-        group = ExamplesGroup.new(examples: examples)
-        categories << Category.new(name: category_config[:name], examples: group)
-      rescue
-        puts "You haven't provided trainingsdata for the category" + category_config[:name]
-        puts "This category was not created."
+    if config.is_a?(Array)
+      puts "The format [{name: name_of_category, path: path_to_trainings_data}] is deprecated and will be removed in future versions. Use the following arguments instead: categories: [name: 'the name', examples:'An example']"
+      config.each do |category_config|
+        begin
+          examples = ExamplesFactory.from_files(category_config[:path])
+          group = ExamplesGroup.new(examples: examples)
+          categories << Category.new(name: category_config[:name], examples: group)
+        rescue
+          puts "You haven't provided trainingsdata for the category" + category_config[:name]
+          puts "This category was not created."
+        end
       end
+      Categories.new(categories: categories)
+    else
+      config.categories.each do |category_config|
+        begin
+          group = ExamplesGroup.new(examples: category_config[:examples])
+          categories << Category.new(name: category_config[:name], examples: group)
+        rescue
+          puts "You haven't provided trainingsdata for the category" + category_config[:name]
+          puts "This category was not created."
+        end
+      end
+      Categories.new(categories: categories)
     end
-
-    Categories.new(categories: categories)
   end
 end
--- spec/naive_bayes_spec.rb
+++ spec/naive_bayes_spec.rb
@@ -5,11 +5,24 @@ describe NaiveText do
     expect(NaiveText::VERSION).not_to be nil
   end
 
-  it 'responds to build' do
-    categories_config = [{name: 'interesting', path: 'spec/training/positive'},
-                         {name: 'boring', path: 'spec/training/negative'}]
-    expect(CategoriesFactory).to receive(:build).with(categories_config)
-    expect(TextClassifier).to receive(:new)
-    expect(NaiveText.build(categories_config))
+  describe 'build' do
+    it 'support the old categories_config syntax' do
+      categories_config = [{name: 'interesting', path: 'spec/training/positive'},
+                           {name: 'boring', path: 'spec/training/negative'}]
+      expect(CategoriesFactory).to receive(:build).with(categories_config)
+      expect(TextClassifier).to receive(:new)
+      expect(NaiveText.build(categories_config))
+    end
+
+    let(:examples1) { ExamplesFactory.from_files 'spec/training/positive' }
+    let(:examples2) { ExamplesFactory.from_files 'spec/training/negative' }
+
+    it 'supports the new categories_config syntax' do
+      categories_config = {categories: [{name: 'interesting', examples: examples1 },
+                           {name: 'boring', example: examples2}]}
+      expect(CategoriesFactory).to receive(:build).with(categories_config)
+      expect(TextClassifier).to receive(:new)
+      expect(NaiveText.build(categories_config))
+    end
   end
 end
